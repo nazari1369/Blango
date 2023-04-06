1ada0ae31184f26485121346820d60d8d7d6b7a5
Behzad Nazari <nazari1369@gmail.com>
2023-04-06 12:15:29 +0000

    Finish custom User model

--- Blango/settings.py
+++ Blango/settings.py
@@ -41,6 +41,7 @@ class Dev(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        "blango_auth",
         'blog',
         "crispy_forms",
         "crispy_bootstrap5",
@@ -139,3 +140,4 @@ class Dev(Configuration):
     SESSION_COOKIE_SAMESITE = 'None'
     CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
     CRISPY_TEMPLATE_PACK = "bootstrap5"
+    AUTH_USER_MODEL = "blango_auth.User"
